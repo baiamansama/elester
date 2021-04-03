1a3aa414641e98f6a6ee2e02e7013197929b5329
Baiaman <baiamansama@gmail.com>
2021-04-03 16:49:19 +0900

    second commit after a while

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js'
 
-const app = express();
+const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors())
 
-app.use('/posts', postRoutes);
+app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://elester123:elester123@cluster0.9e7y3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)
