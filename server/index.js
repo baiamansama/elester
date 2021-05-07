c202031b3983a43eb9caaa7b85964f2eced9a46d
Baiaman <baiamansama@gmail.com>
2021-05-07 17:19:52 +0900

    final state

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
 import postRoutes from './routes/posts.js'
+import userRouter from "./routes/user.js"
 
-const app = express()
+const app = express();
 dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,8 +14,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+app.use("/user", userRouter)
 
-const PORT = process.env.PORT|| 5000
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
